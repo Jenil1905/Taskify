e1d11bba5c63cda4b8ea7fe72f8cf1bb4f3d82cb
Jenil1905 <langaliyajenil@gmail.com>
2025-09-15 18:05:11 +0530

    Fix cookie issue

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -37,7 +37,7 @@ async function signup(req, res) {
     res.cookie('token', token, {
         httpOnly: true,
         secure: true,
-        sameSite: 'strict',
+        sameSite: 'none',
         maxAge: 30*24*60*60*1000 //30 days
     })
 
@@ -81,7 +81,7 @@ async function login(req,res){
         res.cookie('token', token, {
             httpOnly: true,
             secure: true,
-            sameSite: 'strict',
+            sameSite: 'none',
             maxAge: 30*24*60*60*1000 //30 days
         })
 
